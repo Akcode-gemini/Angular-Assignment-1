6cc2832e3e0b75a5132c17c6df7876f3786aef66
Aaliya Khanam <Aaliya.Khanam@Geminisolutions.com>
2023-05-14 12:25:23 +0530

    Assignment 1 Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularAssignment1';
+  title = 'Angular Assignment 1';
+  display = false;
+  count = 0;
+  name = '';
+  nameList: { text: string }[] = [];
+  onClick() {
+    this.display = !this.display;
+    this.count++;
+    //log the click
+    console.log('Button is clicked', this.count, 'times');
+  }
+  Add() {
+    if (this.name) {
+      this.nameList.push({ text: this.name });
+      this.name = '';
+    }
+  }
 }
